02b9556db4adfa8d698af5b66777bcf1c24f01db
boozi2 <saragossi@gmail.com>
2017-11-07 13:58:17 +0200

    Initial Commit

---
+++ HealthKitImporter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HealthKitImporter
+//
+//  Created by boaz saragossi on 11/7/17.
+//  Copyright © 2017 boaz saragossi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
